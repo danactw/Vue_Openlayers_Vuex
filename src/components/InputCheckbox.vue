d7a03ef45694b2763e4321bce37729144476ce7c
danachen <danac.tw@gmail.com>
2022-05-24 15:06:01 +0800

    sidebar in Draw Map

---
+++ src/components/InputCheckbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <input :id="item.title" type="checkbox" v-model="item.checked"/>
+    <label :for="item.title"> {{ item.title }} </label>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style>
+li {
+  list-style: none;
+  margin-top: 10px;
+}
+</style>
--- src/views/DrawMap.vue
+++ src/views/DrawMap.vue
@@ -1,33 +1,53 @@
-<template>
-  <div id="map" class="drawmap" ref="mapContainer"></div>
-  <div class="interaction">
-    <SelectOption :selection="interactionType" itemRef="interactionType"/>
-    <SelectOption :selection="drawType" itemRef="drawType"/>
+<template> 
+  <div class="gridContainer">
+    <div class="grid1">
+      <div class="sidebar">
+        <h2>Interactions</h2>
+        <InputRadio :items="interactionType" itemRef="interactionType"/>
+        <h2>Draw Type</h2>
+        <SelectOption :selection="drawType" itemRef="drawType"/>
+        <h2>Additional</h2>
+        <span>
+          <button @click="clearLastFeature" class="btn">Undo</button>
+          <button @click="clearAllFeatures" class="btn">Clear All</button>
+        </span>
+        <InputCheckbox v-for="option in addOptionToDraw" :key="option" :item="option" />
+      </div>
+    </div>
+    <div class="grid2">
+      <div id="map" class="map" ref="mapContainer"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import { shallowRef, onMounted, markRaw, watchEffect } from 'vue';
 import SelectOption from '@/components/SelectOption.vue';
+import InputRadio from '@/components/InputRadio.vue';
+import InputCheckbox from '@/components/InputCheckbox.vue';
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source';
 // import GeoJSON from 'ol/format/GeoJSON';
-import { Draw } from 'ol/interaction';
-// import { Draw, Modify, Select, Translate } from 'ol/interaction';
+import { Draw, Modify, Select, Translate } from 'ol/interaction';
 import { Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 import { useStore } from 'vuex';
 
 export default {
-  components: { SelectOption },
+  components: { SelectOption, InputRadio, InputCheckbox },
   setup() {
     const store = useStore()
     const mapContainer = shallowRef(null);
     const map = shallowRef(null);
     const interactionType = ['Draw', 'Select', 'Translate', 'Modify']
     const drawType = ['Point', 'LineString', 'Circle', 'Polygon']
+    const addOptionToDraw = [
+      {title: 'Measure', checked: true},
+      {title: 'Measure Segment Length', checked: false},
+      {title: 'Clear Previous Feature', checked: false},
+    ]
 
     const vectorStyle = {
       'Point': new Style({
@@ -83,6 +103,18 @@ export default {
       )
     }
 
+    const select = new Select({
+      wrapX: false,
+    });
+
+    const translate = new Translate({
+      features: select.getFeatures() 
+    })
+
+    const modify = new Modify({
+      features: select.getFeatures(),
+    });
+
     watchEffect(() => {
       const currentDrawType = store.state.selectOptions['drawType']
       const currentDrawTypeIndex = drawType.indexOf(currentDrawType)
@@ -107,24 +139,29 @@ export default {
           zoom: 2,
         }),
       }))
+
       draws.forEach(draw=> {
         if (map.value) map.value.addInteraction(draw)
       })
     })
 
-    return { map, mapContainer, interactionType, drawType }
+    return { map, mapContainer, interactionType, drawType, addOptionToDraw }
   }
 }
 </script>
 
 <style>
-.drawmap {
-  width: 95vw;
-  height: 80vh;
-  margin: 10px auto 5px;
+.btn {
+  padding: 5px;
+  margin-right: 15px;
+  cursor: pointer;
+  display: inline-block;
+  border: none;
+  height: 40px;
+  font-size: 18px;
 }
 
-.interaction {
-  margin-left: 30px;
+.btn:hover {
+  transform: translateY(1px);
 }
 </style>
